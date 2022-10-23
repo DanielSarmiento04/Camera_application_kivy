69eed9741c1d7d23a1fa03bbe4b7534e750d2f40
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2022-10-23 18:04:47 -0500

    Add advanced

--- app/Components/LiveCamera.py
+++ app/Components/LiveCamera.py
@@ -2,6 +2,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+import asyncio
 
 default_path_image = "app/imgs/event_camera.jpeg"
 
@@ -19,9 +20,9 @@ class LiveCamera(BoxLayout):
         self.spacing = 10
         self.orientation = "vertical"
         # Create a camera object
-        self.camera = Camera(play=False, resolution=(720, 480))
+        self.camera = Camera(play=False, resolution=(720, 480), size_hint=(1, 1), pos_hint={'center_x': .5, 'center_y': .5})
         self.camera.source = default_path_image
-        self.add_widget(self.camera)
+        self.add_widget(self.camera, )
         
         # Create a button to open the camere
         self.button_open_close_camera = Button(text='OpenCamera', )
@@ -33,7 +34,13 @@ class LiveCamera(BoxLayout):
         if self.camera.play:
             self.camera.play = False
             self.button_open_close_camera.text = "Open Camera"
+            
+            print("before set source image")
+            # asyncio.wait(1)
             self.camera.source = default_path_image
+            print("after set source image")
         else:
             self.camera.play = True
             self.button_open_close_camera.text = "Close Camera"
+
+    
